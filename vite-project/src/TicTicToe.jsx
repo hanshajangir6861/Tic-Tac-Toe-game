0fad9c789000fb383472a56358fd4e653bc76431
unknown <hanshajangir061@gmail.com>
2023-10-22 09:33:18 -0700

    complete game

--- vite-project/src/TicTicToe.jsx
+++ vite-project/src/TicTicToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './TicTicToe.css'
 import './cross.png' 
 import './circle.png'
@@ -10,7 +10,19 @@ function TicTicToe() {
 
 let [counter , setCounter] = useState(0)
 const [lock , setLock] = useState(false)
+const titleref = useRef(null)
+let box1 = useRef(null)
+let box2 = useRef(null)
+let box3 = useRef(null)
+let box4 = useRef(null)
+let box5 = useRef(null)
+let box6 = useRef(null)
+let box7 = useRef(null)
+let box8 = useRef(null)
+let box9 = useRef(null)
 
+
+let boxarray = [box1,box2,box3,box4,box5,box6,box7,box8,box9]
 
 const toggle = (e, num)=>{
 if(lock){
@@ -40,35 +52,98 @@ else{
   data[num] = "o"
   setCounter(++counter)
 }
+checkwin()
+}
+
+const checkwin = ()=>{
+  if(data[0]===data[1] && data[1]===data[2] && data[2] !=="")
+  {
+won(data[2])
+  }
+  else if(data[3]===data[4] && data[4]===data[5] && data[5] !=="")
+  {
+    won(data[5])
+  }
+  else if(data[6]===data[7] && data[7]===data[8] && data[8] !=="")
+  {
+    won(data[8])
+  }
+  else if(data[0]===data[3] && data[3]===data[6] && data[6] !=="")
+  {
+    won(data[6])
+  }
+  else if(data[1]===data[4] && data[4]===data[7] && data[7] !=="")
+  {
+    won(data[7])
+  }
+  else if(data[2]===data[5] && data[5]===data[8] && data[8] !=="")
+  {
+    won(data[8])
+  }
+  else if(data[0]===data[4] && data[4]===data[8] && data[8] !=="")
+  {
+    won(data[8])
+  }
+  else if(data[0]===data[1] && data[1]===data[2] && data[2] !=="")
+  {
+    won(data[2])
+  }
+  else if(data[2]===data[4] && data[4]===data[6] && data[6] !=="")
+  {
+    won(data[6])
+  }
+}
+
+const won = (winner)  =>{
+  setLock(true);
+  if(winner=== "x")
+  {
+    titleref.current.innerHTML = `Congratulations : X wins`
+  }
+  else(winner=== "o")
+  {
+    titleref.current.innerHTML = `Congratulations : O wins`
+
+  }
+
 }
 
 
+const reset = () =>{
+  setLock(false);
+  data = ["","","","","","","","",""];
+  titleref.current.innerHTML = 'Tic Tac Toe <span> React </span>'
+  boxarray.map((e)=>{
+    e.current.innerHTML = "";
+  })
+}
+
   return (
     <div className='container'>
-      <h1 className='title'> Tic Tac Toe game in <span>React</span></h1>
+      <h1 className='title' ref={titleref}> Tic Tac Toe game in <span>React</span></h1>
 
       <div className='board'>
 
         <div id="row1">
-          <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
+          <div className="boxes" ref={box1} onClick={(e)=>{toggle(e,0)}}></div>
+          <div className="boxes"ref={box2} onClick={(e)=>{toggle(e,1)}}></div>
+          <div className="boxes" ref={box3} onClick={(e)=>{toggle(e,2)}}></div>
         </div>
 
         <div id="row2">
-          <div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
+          <div className="boxes"  ref={box4} onClick={(e)=>{toggle(e,3)}}></div>
+          <div className="boxes"  ref={box5} onClick={(e)=>{toggle(e,4)}}></div>
+          <div className="boxes" ref={box6} onClick={(e)=>{toggle(e,5)}}></div>
         </div>
 
         <div id="row3">
-          <div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
-          <div className="boxes" onClick={(e)=>{toggle(e,8)}}></div>
+          <div className="boxes" ref={box7} onClick={(e)=>{toggle(e,6)}}></div>
+          <div className="boxes" ref={box8} onClick={(e)=>{toggle(e,7)}}></div>
+          <div className="boxes" ref={box9} onClick={(e)=>{toggle(e,8)}}></div>
         </div>
 
       </div>
-      <button className='reset'>React</button>
+      <button className='reset' onClick={()=>{reset()}}>React</button>
     </div>
   )
 }
